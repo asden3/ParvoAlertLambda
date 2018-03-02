fdec087af27261a6a1c66a6ed825d5ec9ddcba5f
VendableFall <info@leet.cloud>
2018-03-02 20:02:13 +1000

    delete user proxy code

--- cognitoDeleteUser.js
+++ cognitoDeleteUser.js
@@ -1,4 +1,22 @@
-exports.handler = function(event, context) {
-    // TODO implement
-    context.done(null, 'Hello from Lambda');
+exports.handler = function(event, context) 
+{
+    var params = 
+    {
+        AccessToken: 'STRING_VALUE' /* required */
+    };
+
+    cognitoidentityserviceprovider.deleteUser(params, function(err, data) {
+    if (err) 
+    {
+        console.log(err, err.stack); // an error occurred
+        callback(err);
+    }
+    else
+    {
+            console.log(data);           // successful response
+            callback(null, data);
+    }
+    });
 };
+
+
